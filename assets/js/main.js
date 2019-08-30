cb378cf9f735ed06f1aa62058f448433c7a9fcb1
rathanont <rathanont.s@gmail.com>
2019-08-30 23:13:17 +0700

    fixed reset-password

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,16 @@
-$(function() {
+$(function () {
   // Show sign-up modal and hide sign-in modal
-  $("#signUpModal").on("show.bs.modal", function(e) {
+  $("#signUpModal").on("show.bs.modal", function (e) {
     $("#signInModal").modal("hide");
   });
 
   // Show sign-in modal and hide sign-up modal
-  $("#signInModal").on("show.bs.modal", function(e) {
+  $("#signInModal").on("show.bs.modal", function (e) {
     $("#signUpModal").modal("hide");
   });
 
   // Send reset password
-  $("#sendResetPassword").click(function() {
+  $("#sendResetPassword").click(function () {
     Swal.fire({
       html: `<div class="text-center">
         <h5>Success!</h5>
@@ -22,7 +22,7 @@ $(function() {
   });
 
   // Sign-up success
-  $("#btnSignUp").click(function() {
+  $("#btnSignUp").click(function () {
     Swal.fire({
       html: `<div class="text-center">
         <h5>Success!</h5>
@@ -33,3 +33,22 @@ $(function() {
     $("#signUpModal").modal("hide");
   });
 });
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict';
+  window.addEventListener('load', function () {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
